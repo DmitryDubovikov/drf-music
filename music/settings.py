8ef936cb8e5b445b00f52badc5a50ab89d1d985c
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-02-03 18:15:49 +0300

    swagger docs with drf_spectacular

--- music/settings.py
+++ music/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external packages
     "rest_framework",
+    "drf_spectacular",
+    # internal apps
     "musicAPI.apps.MusicapiConfig",
 ]
 
@@ -123,3 +126,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Music",
+    "DESCRIPTION": "project description: singer, album, song, content",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # OTHER SETTINGS
+}
