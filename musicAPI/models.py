55a4df0f97cf958ee279791f3b4c33bec8182b98
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-02-03 15:36:25 +0300

    basic setup and models

---
+++ musicAPI/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Singer(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    singer = models.ForeignKey(Singer, on_delete=models.CASCADE)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Content(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    track_number = models.IntegerField()
